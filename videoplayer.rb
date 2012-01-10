05062a283284886c3fe76f49fa65f78d060679fc
Marc Hofer <mhofer@thoughtworks.com>
2012-01-10 15:33:58 +0000

    add zoom to mplayer

--- videoplayer.rb
+++ videoplayer.rb
@@ -6,7 +6,7 @@ set :port, 59623
 
 get '/play' do
   unescapedUrl = CGI.unescape(params[:p])
-  `mplayer -fs -cache 8192 "#{unescapedUrl}" &`
+  `mplayer -fs -zoom -cache 8192 "#{unescapedUrl}" &`
   "Just played #{unescapedUrl}"
 end
 
