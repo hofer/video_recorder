e1e7ab6b2ee16d5732cef81cf1e214a80efdb4a1
Marc Hofer <mhofer@thoughtworks.com>
2012-01-10 17:24:20 +0000

    Xvid library added.

--- rakefile.rb
+++ rakefile.rb
@@ -7,9 +7,17 @@ end
 
 desc "Install screen recorder."
 task :install_recorder => :install_packages do
-	sh "wget http://ffmpeg.org/releases/ffmpeg-0.8.6.tar.gz"
+
+  sh "wget http://ftp.br.debian.org/debian-multimedia/pool/main/x/xvidcore/xvidcore_1.3.2.orig.tar.gz"
+  sh "tar -xvzf xvidcore_1.3.2.orig.tar.gz"
+  sh "cd xvidcore-1.3.2/build/generic && ./configure"
+  sh "cd xvidcore-1.3.2/build/generic && make"
+  sh "cd xvidcore-1.3.2/build/generic && sudo make install"
+  sh "rm -rf xvidcore*"
+
+  sh "wget http://ffmpeg.org/releases/ffmpeg-0.8.6.tar.gz"
   sh "tar -xvzf ffmpeg-0.8.6.tar.gz" 
-  sh "cd ffmpeg-0.8.6 && ./configure --enable-gpl --enable-x11grab --enable-nonfree"
+  sh "cd ffmpeg-0.8.6 && ./configure --enable-gpl --enable-x11grab --enable-nonfree --enable-libxvid"
   sh "cd ffmpeg-0.8.6 && sudo make clean install"
   sh "rm -rf ffmpeg-*"
 end
@@ -18,7 +26,7 @@ desc "Install player."
 task :install_player => :install_packages do
   
   # Install mplayer
-	sh "wget --no-check-certificate https://github.com/downloads/hofer/video_recorder/mplayer.tar.gz"
+  sh "wget --no-check-certificate https://github.com/downloads/hofer/video_recorder/mplayer.tar.gz"
   sh "tar -xvzf mplayer.tar.gz"
   sh "cd mplayer-export-2012-01-09/ && ./configure"
   sh "cd mplayer-export-2012-01-09/ && sudo make clean install"
@@ -32,27 +40,27 @@ end
 
 desc "Play recorded video"
 task :play do
-	puts "Want to play ...."
+  puts "Want to play ...."
   sh "mplayer -fs out.mpg"
 end
 
 desc "Play on tv"
 task :play_on_tv do
-  sh "scp out.mpg repo.tools:/var/www/html/out.mpg"
-  sh "wget http://172.18.10.249:59623/play?p='http://repo.tools/out.mpg'"
+  sh "scp out.mp4 repo.tools:/var/www/html/out.mp4"
+  sh "wget http://172.18.10.249:59623/play?p='http://repo.tools/out.mp4'"
 end
 
 desc "Record from screen"
 task :record_start do
-	puts "Start recording from screen :0"
-	sh "rm -f out.mpg"
-	sh "ffmpeg -f x11grab -s 1000x700 -i :0 out.mpg > /dev/null 2>&1 &"
+  puts "Start recording from screen :0"
+  sh "rm -f out.mp4"
+  sh "ffmpeg -f x11grab -s 1024x768 -i :0 -vcodec libxvid -r 25 -b 4000k -f mp4 out.mp4 > /dev/null 2>&1 &"
 end
 
 desc "Stop recording"
 task :record_stop do
-	puts "Stop recording from screen :99"
-	sh "killall ffmpeg"
+  puts "Stop recording from screen :99"
+  sh "killall ffmpeg"
 end
 
 desc "Record a test"
@@ -76,12 +84,10 @@ end
 
 desc "Recording a test"
 task :record_test => [:test_prepare, :record_start, :test_run, :record_stop] do
-	puts "Start recording a test"
 end
 
 task :cleanup do
-	sh "sudo rm -rf artifacts core marklogic tests out.mpg"
+  sh "sudo rm -rf artifacts core marklogic tests out.mp4"
 end
 
 # http://172.18.12.111:/play?p=http://172.18.12.111:8085/out.mpg
-# http://ftp.br.debian.org/debian-multimedia/pool/main/x/xvidcore/xvidcore_1.3.2.orig.tar.gz
